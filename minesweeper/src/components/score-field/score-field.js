5b636df53ec14ae2166bd885e29e42796b966c11
Vladimir <privezenoff@yandex.ru>
2021-03-03 19:16:52 +0300

    feat: add score-field component

---
+++ minesweeper/src/components/score-field/score-field.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class ScoreField extends Component {
+  render() {
+    return (
+      <div>
+        <div>1. 20s</div>
+        <div>1. 25s</div>
+        <div>1. 26s</div>
+      </div>
+    )
+  }
+}
