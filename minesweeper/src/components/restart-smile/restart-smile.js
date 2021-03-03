12d6eace07eb5a9166360dce4229919dc6f8e5ab
Vladimir <privezenoff@yandex.ru>
2021-03-03 19:16:40 +0300

    feat: add restart-smile component

---
+++ minesweeper/src/components/restart-smile/restart-smile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './restart-smile.scss';
+
+const RestartSmile = () => {
+  return (
+    <button className="button button_restart-good"></button>
+  );
+}
+
+export default RestartSmile;
